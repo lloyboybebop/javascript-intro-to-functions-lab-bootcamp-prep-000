f164a1395ef2aea3937a2649ad8e05a8081ccd43
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-06-10 20:58:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else {
+  } else if (string === string.toUpperCase()){
     return "YES INDEED!";
+  }
+    else {
+    return "I love you, Grandma.";
   }
 }
